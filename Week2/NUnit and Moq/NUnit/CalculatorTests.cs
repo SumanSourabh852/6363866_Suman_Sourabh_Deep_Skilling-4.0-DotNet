d88e8158c4f749bfedac7526567e6e2fab941054
SumanSourabh852 <sumansourabh852.90@gmail.com>
2025-06-29 22:50:00 +0530

    Add files via upload

---
+++ Week2/NUnit and Moq/NUnit/CalculatorTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using CalcLibrary;
+
+namespace CalcLibrary.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private Calculator _calculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _calculator = new Calculator();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _calculator = null;
+        }
+
+        [Test]
+        [TestCase(2, 3, 5)]
+        [TestCase(0, 0, 0)]
+        [TestCase(-1, -1, -2)]
+        public void Add_AddsTwoNumbers_ReturnsExpectedResult(int a, int b, int expected)
+        {
+            var result = _calculator.Add(a, b);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [Ignore("Demo: Ignored test")]
+        public void IgnoredTest()
+        {
+            Assert.Fail("This should not run.");
+        }
+    }
+}
